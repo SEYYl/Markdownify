5711ba9d767fbcf16ac661d19d0e7e76c33d0230
SEYYl <1719099363@qq.com>
2026-05-16 07:37:09 +0800

    修复Render部署问题

--- app.py
+++ app.py
@@ -61,4 +61,6 @@ def convert_url():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
